55f09c15ae9f4535547edf88595b3a529530b88c
Andrew Harpin <mini_pickup_turbo@hotmail.com>
2023-02-26 11:07:24 +0000

    Function updates, mostly TODOs

--- C/function/function.c
+++ C/function/function.c
@@ -4,21 +4,26 @@ uint32_t nthreads = 4;
 uint8_t show_truth_table = 0;
 uint32_t nargs = 6;
 
-uint8_t function (uint8_t input[])
+// TODO: Understand this, looks to be some logical combination operation
+uint8_t function(uint8_t input[])
 {
 	return (uint8_t) ((input[0] & input[1]) ^ (input[2] & input[3]) ^ (input[4] & input[5]));
+
 }
 
+// TODO: This looks like it needs to be updated to support operating system
 uint32_t get_nthreads(void)
 {
 	return nthreads;
 }
 
+// TODO: This looks to do nothing
 uint8_t get_show_truth_table(void)
 {
 	return show_truth_table;
 }
 
+// TODO: This likely needs to be updated to support file under analysis
 uint32_t get_nargs(void)
 {
 	return nargs;
--- C/function/function.h
+++ C/function/function.h
@@ -3,6 +3,7 @@
 
 #include "stdint.h"
 
+// TODO: Understand what this poorly name "function" does
 uint8_t function(uint8_t input[]);
 uint32_t get_nthreads(void);
 uint8_t get_show_truth_table(void);
